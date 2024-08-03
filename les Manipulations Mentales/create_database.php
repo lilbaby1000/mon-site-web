2ceecf0da5d9958fc9ecdd59dc00ac4634471975
lilbaby1000 <cabromcriss1000@gmail.com>
2024-08-03 16:34:25 +0000

    Add files via upload

---
+++ les Manipulations Mentales/create_database.php
@@ -0,0 +1,8 @@
+<?php
+// Connexion à la base de données
+$servername = "localhost";
+$username = "Ouattara Ibrahim";
+$password = "Iotresbonne1000";
+$dbname = "your_database";
+
+$conn = new mysqli
